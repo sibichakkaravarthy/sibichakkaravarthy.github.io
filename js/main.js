81f0d2566b5f8523378d2895637b885f9ebdb846
Sibi Chakkaravarthy S <sb.sibi@gmail.com>
2021-08-17 10:34:04 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -61,27 +61,27 @@ function update() {
         temp = 5;
     }
     if (temp == 0) {
-        document.getElementById('m1').innerHTML = '<a href="/sb/aboutMe.html"><i class="fas fa-chevron-right"></i></i></a>';
-        document.getElementById('m2').innerHTML = '<a href="/sb/gallery.html"><i class="fas fa-chevron-left"></i></i></i></a>';
+        document.getElementById('m1').innerHTML = '<a href="/aboutMe.html"><i class="fas fa-chevron-right"></i></i></a>';
+        document.getElementById('m2').innerHTML = '<a href="/gallery.html"><i class="fas fa-chevron-left"></i></i></i></a>';
     }
     if (temp == 1) {
-        document.getElementById('m1').innerHTML = '<a href="/sb/researchGrant.html"><i class="fas fa-chevron-right"></i></i></a>';
-        document.getElementById('m2').innerHTML = '<a href="/sb/index.html"><i class="fas fa-chevron-left"></i></i></i></a>';
+        document.getElementById('m1').innerHTML = '<a href="/researchGrant.html"><i class="fas fa-chevron-right"></i></i></a>';
+        document.getElementById('m2').innerHTML = '<a href="/index.html"><i class="fas fa-chevron-left"></i></i></i></a>';
     }
     if (temp == 2) {
-        document.getElementById('m1').innerHTML = '<a href="/sb/researchProjects.html"><i class="fas fa-chevron-right"></i></i></a>';
+        document.getElementById('m1').innerHTML = '<a href="/researchProjects.html"><i class="fas fa-chevron-right"></i></i></a>';
         document.getElementById('m2').innerHTML = '<a href="/sb/aboutMe.html"><i class="fas fa-chevron-left"></i></i></i></a>';
     }
     if (temp == 3) {
-        document.getElementById('m1').innerHTML = '<a href="/sb/publications.html"><i class="fas fa-chevron-right"></i></i></a>';
-        document.getElementById('m2').innerHTML = '<a href="/sb/researchGrant.html"><i class="fas fa-chevron-left"></i></i></i></a>';
+        document.getElementById('m1').innerHTML = '<a href="/publications.html"><i class="fas fa-chevron-right"></i></i></a>';
+        document.getElementById('m2').innerHTML = '<a href="/researchGrant.html"><i class="fas fa-chevron-left"></i></i></i></a>';
     }
     if (temp == 4) {
-        document.getElementById('m1').innerHTML = '<a href="/sb/gallery.html"><i class="fas fa-chevron-right"></i></i></a>';
+        document.getElementById('m1').innerHTML = '<a href="/gallery.html"><i class="fas fa-chevron-right"></i></i></a>';
         document.getElementById('m2').innerHTML = '<a href="/sb/researchProjects.html"><i class="fas fa-chevron-left"></i></i></i></a>';
     }
     if (temp == 5) {
-        document.getElementById('m1').innerHTML = '<a href="/sb/index.html"><i class="fas fa-chevron-right"></i></i></i></a>';
-        document.getElementById('m2').innerHTML = '<a href="/sb/publications.html"><i class="fas fa-chevron-left"></i></i></a>';
+        document.getElementById('m1').innerHTML = '<a href="/index.html"><i class="fas fa-chevron-right"></i></i></i></a>';
+        document.getElementById('m2').innerHTML = '<a href="/publications.html"><i class="fas fa-chevron-left"></i></i></a>';
     }
 }
