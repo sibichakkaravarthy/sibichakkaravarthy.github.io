ff6af29dfd77636f8758f6b8f15181e2ecf563bf
Sibi Chakkaravarthy S <sb.sibi@gmail.com>
2021-08-17 10:22:15 +0530

    update v.32

---
+++ js/main.js
@@ -0,0 +1,87 @@
+const swup = new Swup();
+update();
+swup.on('contentReplaced', update);
+var temp = 0;
+
+function update() {
+    if (window.location.href == 'https://sibichakkaravarthy.github.io/index.html') {
+        document.getElementById('mm0').classList.add('active');
+        document.getElementById('mm1').classList.remove('active');
+        document.getElementById('mm2').classList.remove('active');
+        document.getElementById('mm3').classList.remove('active');
+        document.getElementById('mm4').classList.remove('active');
+        document.getElementById('mm5').classList.remove('active');
+
+
+        temp = 0;
+    }
+    if (window.location.href == 'https://sibichakkaravarthy.github.io/aboutMe.html') {
+        document.getElementById('mm0').classList.remove('active');
+        document.getElementById('mm1').classList.add('active');
+        document.getElementById('mm2').classList.remove('active');
+        document.getElementById('mm3').classList.remove('active');
+        document.getElementById('mm4').classList.remove('active');
+        document.getElementById('mm5').classList.remove('active');
+        temp = 1;
+    }
+    if (window.location.href == 'https://sibichakkaravarthy.github.io/researchGrant.html') {
+        document.getElementById('mm0').classList.remove('active');
+        document.getElementById('mm1').classList.remove('active');
+        document.getElementById('mm2').classList.add('active');
+        document.getElementById('mm3').classList.remove('active');
+        document.getElementById('mm4').classList.remove('active');
+        document.getElementById('mm5').classList.remove('active');
+        temp = 2;
+    }
+    if (window.location.href == 'https://sibichakkaravarthy.github.io/researchProjects.html') {
+        document.getElementById('mm0').classList.remove('active');
+        document.getElementById('mm1').classList.remove('active');
+        document.getElementById('mm2').classList.remove('active');
+        document.getElementById('mm3').classList.add('active');
+        document.getElementById('mm4').classList.remove('active');
+        document.getElementById('mm5').classList.remove('active');
+        temp = 3;
+    }
+    if (window.location.href == 'https://sibichakkaravarthy.github.io/publications.html') {
+        document.getElementById('mm0').classList.remove('active');
+        document.getElementById('mm1').classList.remove('active');
+        document.getElementById('mm2').classList.remove('active');
+        document.getElementById('mm3').classList.remove('active');
+        document.getElementById('mm4').classList.add('active');
+        document.getElementById('mm5').classList.remove('active');
+        temp = 4;
+    }
+    if (window.location.href == 'https://sibichakkaravarthy.github.io/gallery.html') {
+        document.getElementById('mm0').classList.remove('active');
+        document.getElementById('mm1').classList.remove('active');
+        document.getElementById('mm2').classList.remove('active');
+        document.getElementById('mm3').classList.remove('active');
+        document.getElementById('mm4').classList.remove('active');
+        document.getElementById('mm5').classList.add('active');
+        temp = 5;
+    }
+    if (temp == 0) {
+        document.getElementById('m1').innerHTML = '<a href="/sb/aboutMe.html"><i class="fas fa-chevron-right"></i></i></a>';
+        document.getElementById('m2').innerHTML = '<a href="/sb/gallery.html"><i class="fas fa-chevron-left"></i></i></i></a>';
+    }
+    if (temp == 1) {
+        document.getElementById('m1').innerHTML = '<a href="/sb/researchGrant.html"><i class="fas fa-chevron-right"></i></i></a>';
+        document.getElementById('m2').innerHTML = '<a href="/sb/index.html"><i class="fas fa-chevron-left"></i></i></i></a>';
+    }
+    if (temp == 2) {
+        document.getElementById('m1').innerHTML = '<a href="/sb/researchProjects.html"><i class="fas fa-chevron-right"></i></i></a>';
+        document.getElementById('m2').innerHTML = '<a href="/sb/aboutMe.html"><i class="fas fa-chevron-left"></i></i></i></a>';
+    }
+    if (temp == 3) {
+        document.getElementById('m1').innerHTML = '<a href="/sb/publications.html"><i class="fas fa-chevron-right"></i></i></a>';
+        document.getElementById('m2').innerHTML = '<a href="/sb/researchGrant.html"><i class="fas fa-chevron-left"></i></i></i></a>';
+    }
+    if (temp == 4) {
+        document.getElementById('m1').innerHTML = '<a href="/sb/gallery.html"><i class="fas fa-chevron-right"></i></i></a>';
+        document.getElementById('m2').innerHTML = '<a href="/sb/researchProjects.html"><i class="fas fa-chevron-left"></i></i></i></a>';
+    }
+    if (temp == 5) {
+        document.getElementById('m1').innerHTML = '<a href="/sb/index.html"><i class="fas fa-chevron-right"></i></i></i></a>';
+        document.getElementById('m2').innerHTML = '<a href="/sb/publications.html"><i class="fas fa-chevron-left"></i></i></a>';
+    }
+}
